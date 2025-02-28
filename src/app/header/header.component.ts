6d3b49d599f496e076d0621130d22dd69610e34f
joangnz <joangnz@gmail.com>
2025-02-28 18:43:35 +0100

    Idk

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { JordanSvgComponent } from '../jordan-svg/jordan-svg.component';
+import { ConverseSvgComponent } from '../converse-svg/converse-svg.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [JordanSvgComponent, ConverseSvgComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
