7c0990927e47fc4d10751a72496b406469c0fd19
joangnz <joangnz@gmail.com>
2025-03-12 17:06:54 +0100

    Improving products showpage

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsService } from '../products-service/products.service';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'app-products',
   imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
+  products: Signal<Product[]>;
 
+  constructor(private productService: ProductsService) {
+    this.products = this.productService.loadProducts();
+  }
+
+  ngOnInit(): void {
+    this.productService.loadProducts();
+  }
 }
